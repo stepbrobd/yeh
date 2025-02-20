12d37560b77ef8d0a5ab2d01e63ac67ed0aa6de3
Yifei Sun <ysun@hey.com>
2025-02-20 13:51:39 -0500

    lib/hey: make api a singleton

--- bin/main.ml
+++ bin/main.ml
@@ -4,6 +4,7 @@ module Hey = Yeh.Hey.Hey
 
 let () =
   let cfg = Config.instance () in
+  printf "domain: %s\n" cfg.domain;
   printf "user: %s\n" cfg.user;
   printf "pass: %s\n" cfg.pass;
   printf "token: %s\n" cfg.token;
@@ -11,12 +12,18 @@ let () =
   printf "imap port: %d\n" cfg.imap.port;
   printf "smtp host: %s\n" cfg.smtp.host;
   printf "smtp port: %d\n" cfg.smtp.port;
-  let module H = Hey (struct end) in
-  printf "cable: %s\n" H.Api.cable;
-  printf "imbox: %s\n" H.Api.imbox;
-  printf "drafts: %s\n" H.Api.Topics.drafts;
-  printf "sent: %s\n" H.Api.Topics.sent;
-  printf "spam: %s\n" H.Api.Topics.spam;
-  printf "trash: %s\n" H.Api.Topics.trash;
-  printf "everything: %s\n" H.Api.Topics.everything
+  let module H =
+    Hey (struct
+      let token = cfg.token
+      let domain = cfg.domain
+    end)
+  in
+  let api = H.Api.instance () in
+  printf "cable: %s\n" api.cable;
+  printf "imbox: %s\n" api.imbox;
+  printf "drafts: %s\n" api.topics.drafts;
+  printf "sent: %s\n" api.topics.sent;
+  printf "spam: %s\n" api.topics.spam;
+  printf "trash: %s\n" api.topics.trash;
+  printf "everything: %s\n" api.topics.everything
 ;;
--- lib/hey.ml
+++ lib/hey.ml
@@ -2,33 +2,80 @@ open! Core
 module Protocol = Protocol.Protocol
 
 module Hey (Args : sig
-    (* val token : string *)
+    val domain : string
+    val token : string
   end) =
 struct
-  let domain = "app.hey.com"
-
-  module Endpoint = struct
-    let mk ?(protocol = Protocol.HTTPS) domain path =
-      Printf.sprintf "%s://%s%s" (Protocol.to_string protocol) domain path
-    ;;
-  end
-
   module Api = struct
-    let cable = Endpoint.mk ~protocol:Protocol.WSS domain "/cable"
-    let imbox = Endpoint.mk domain "/imbox"
+    let mk ?(protocol = Protocol.HTTPS) path =
+      Printf.sprintf "%s://%s%s" (Protocol.to_string protocol) Args.domain path
+    ;;
+
+    let invoke uri ?(verb = Protocol.HTTPS.GET) ?(body = "") =
+      match Protocol.of_string uri with
+      | Some Protocol.HTTPS -> ""
+      | Some Protocol.WSS -> ""
+      | _ -> failwith "unsupported"
+    ;;
 
     module Topics = struct
       (** [mk t] creates an endpoint for a topic where [t] can be either a named topic or an email id *)
-      let mk t = Endpoint.mk domain (Printf.sprintf "/topics/%s" t)
+      let topic t = mk (Printf.sprintf "/topics/%s" t)
 
       (** [attachments t] creates an endpoint for all attachments relevant to an email with id [t] *)
-      let attachments t = Endpoint.mk domain (Printf.sprintf "/topics/%s/attachments" t)
+      let attachments t = mk (Printf.sprintf "/topics/%s/attachments" t)
 
-      let drafts = mk "drafts"
-      let sent = mk "sent"
-      let spam = mk "spam"
-      let trash = mk "trash"
-      let everything = mk "everything"
+      type t =
+        { drafts : string
+        ; sent : string
+        ; spam : string
+        ; trash : string
+        ; everything : string
+        ; topic : string -> string
+        ; attachments : string -> string
+        }
+
+      let create () =
+        { drafts = topic "drafts"
+        ; sent = topic "sent"
+        ; spam = topic "spam"
+        ; trash = topic "trash"
+        ; everything = topic "everything"
+        ; topic
+        ; attachments
+        }
+      ;;
     end
+
+    type t =
+      { cable : string
+      ; imbox : string
+      ; topics : Topics.t
+      ; invoke : string -> ?verb:Protocol.HTTPS.t -> ?body:string -> string
+      }
+
+    let instance = ref None
+
+    let create () =
+      { cable = mk ~protocol:Protocol.WSS "/cable"
+      ; imbox = mk "/imbox"
+      ; topics = Topics.create ()
+      ; invoke
+      }
+    ;;
+
+    let init () =
+      match !instance with
+      | Some _ -> ()
+      | None -> instance := Some (create ())
+    ;;
+
+    let instance () =
+      match !instance with
+      | Some api -> api
+      | None ->
+        init ();
+        Option.value_exn !instance
+    ;;
   end
 end
