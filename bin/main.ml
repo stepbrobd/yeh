ce58d03d6483f5bd66aa5dcb137cc0eb908ed76a
Yifei Sun <ysun@hey.com>
2025-02-20 12:17:53 -0500

    bin/main: test endpoints

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,6 @@
 open! Core
 module Config = Yeh.Config.Config
+module Hey = Yeh.Hey.Hey
 
 let () =
   let cfg = Config.instance () in
@@ -9,5 +10,13 @@ let () =
   printf "imap host: %s\n" cfg.imap.host;
   printf "imap port: %d\n" cfg.imap.port;
   printf "smtp host: %s\n" cfg.smtp.host;
-  printf "smtp port: %d\n" cfg.smtp.port
+  printf "smtp port: %d\n" cfg.smtp.port;
+  let module H = Hey (struct end) in
+  printf "cable: %s\n" H.Api.cable;
+  printf "imbox: %s\n" H.Api.imbox;
+  printf "drafts: %s\n" H.Api.Topics.drafts;
+  printf "sent: %s\n" H.Api.Topics.sent;
+  printf "spam: %s\n" H.Api.Topics.spam;
+  printf "trash: %s\n" H.Api.Topics.trash;
+  printf "everything: %s\n" H.Api.Topics.everything
 ;;
