26311edaaf3eb90b0970449d0987d4755a62051e
Yifei Sun <ysun@hey.com>
2025-02-20 09:59:52 -0500

    bin/main: test config parsing

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,13 @@
 open! Core
+module Config = Yeh.Config.Config
 
-let () = print_endline "Hello, World!"
+let () =
+  let cfg = Config.instance () in
+  printf "user: %s\n" cfg.user;
+  printf "pass: %s\n" cfg.pass;
+  printf "token: %s\n" cfg.token;
+  printf "imap host: %s\n" cfg.imap.host;
+  printf "imap port: %d\n" cfg.imap.port;
+  printf "smtp host: %s\n" cfg.smtp.host;
+  printf "smtp port: %d\n" cfg.smtp.port
+;;
