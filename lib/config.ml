0ce0e35315ed710cadbd9f522ca87426b561e56f
Yifei Sun <ysun@hey.com>
2025-02-20 09:48:38 -0500

    lib/config: singleton config parsing

---
+++ lib/config.ml
@@ -0,0 +1,83 @@
+open! Core
+
+module Config = struct
+  let prefix = "YEH"
+
+  module Mail (Args : sig
+      val prefix : string
+      val default_host : string
+      val default_host_env : string
+      val default_port : int
+      val default_port_env : string
+    end) =
+  struct
+    type t =
+      { host : string
+      ; port : int
+      }
+
+    let default = { host = Args.default_host; port = Args.default_port }
+
+    let of_env () =
+      let host =
+        Sys.getenv (Args.prefix ^ Args.default_host_env)
+        |> Option.value ~default:default.host
+      in
+      let port =
+        Sys.getenv (Args.prefix ^ Args.default_port_env)
+        |> Option.map ~f:Int.of_string
+        |> Option.value ~default:default.port
+      in
+      { host; port }
+    ;;
+  end
+
+  module Imap = Mail (struct
+      let prefix = prefix ^ "_IMAP"
+      let default_host = "127.0.0.1"
+      let default_host_env = "_HOST"
+      let default_port = 993
+      let default_port_env = "_PORT"
+    end)
+
+  module Smtp = Mail (struct
+      let prefix = prefix ^ "_SMTP"
+      let default_host = "127.0.0.1"
+      let default_host_env = "_HOST"
+      let default_port = 465
+      let default_port_env = "_PORT"
+    end)
+
+  type t =
+    { user : string
+    ; pass : string
+    ; token : string
+    ; imap : Imap.t
+    ; smtp : Smtp.t
+    }
+
+  let instance = ref None
+
+  let of_env () =
+    { user = Sys.getenv_exn (prefix ^ "_USER")
+    ; pass = Sys.getenv_exn (prefix ^ "_PASS")
+    ; token = Sys.getenv_exn (prefix ^ "_TOKEN")
+    ; imap = Imap.of_env ()
+    ; smtp = Smtp.of_env ()
+    }
+  ;;
+
+  let init () =
+    match !instance with
+    | Some _ -> ()
+    | None -> instance := Some (of_env ())
+  ;;
+
+  let instance () =
+    match !instance with
+    | Some config -> config
+    | None ->
+      init ();
+      Option.value_exn !instance
+  ;;
+end
