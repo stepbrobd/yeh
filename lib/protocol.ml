83412424dd9a4a4cc4915ee24257bd8c24d36ef8
Yifei Sun <ysun@hey.com>
2025-02-20 13:51:07 -0500

    lib/protocol: add matching and to string

--- lib/protocol.ml
+++ lib/protocol.ml
@@ -1,6 +1,26 @@
 open! Core
 
 module Protocol = struct
+  module HTTPS = struct
+    type t =
+      | DELETE
+      | GET
+      | POST
+
+    let to_string = function
+      | DELETE -> "DELETE"
+      | GET -> "GET"
+      | POST -> "POST"
+    ;;
+
+    let of_string = function
+      | "DELETE" -> Some DELETE
+      | "GET" -> Some GET
+      | "POST" -> Some POST
+      | _ -> None
+    ;;
+  end
+
   type t =
     | HTTPS
     | IMAP
@@ -12,5 +32,14 @@ module Protocol = struct
     | IMAP -> "imap"
     | SMTP -> "smtp"
     | WSS -> "wss"
+  ;;
+
+  let of_string uri =
+    match Uri.scheme (Uri.of_string uri) with
+    | Some "https" -> Some HTTPS
+    | Some "imap" -> Some IMAP
+    | Some "smtp" -> Some SMTP
+    | Some "wss" -> Some WSS
+    | _ -> None
   ;;
 end
