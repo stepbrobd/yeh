f6ab57a71821e7e4c9e85229eb1235c614fcc9b9
Yifei Sun <ysun@hey.com>
2025-02-23 16:12:08 -0500

    bin/main: try mrmime
    we prob dont need parsing at all in the end?
    just directly fwd raw email in string to downstream imap client

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,35 @@
 open! Core
 
+(* https://ocaml.org/p/mrmime/latest/doc/Mrmime/Mail/index.html *)
+let rec print_part (part : string Mrmime.Mail.t) =
+  match part with
+  | Leaf c -> printf "content:\n%s\n" c
+  | Multipart p ->
+    printf "multipart:\n";
+    List.iter p ~f:(fun (h, oc) ->
+      printf "multipart header:-\n";
+      List.iter (Mrmime.Header.to_list h) ~f:(Mrmime.Field.pp Format.std_formatter);
+      match oc with
+      | Some p -> print_part p
+      | None -> ())
+  | Message (h, c) ->
+    printf "embedded message:\n";
+    printf "embedded message header:\n";
+    List.iter (Mrmime.Header.to_list h) ~f:(Mrmime.Field.pp Format.std_formatter);
+    print_part c
+;;
+
+(* https://ocaml.org/p/angstrom/latest/doc/Angstrom/index.html *)
+let parse_print raw =
+  let parser = Mrmime.Mail.mail None in
+  match Angstrom.parse_string ~consume:All parser raw with
+  | Ok (h, c) ->
+    printf "header:\n";
+    List.iter (Mrmime.Header.to_list h) ~f:(Mrmime.Field.pp Format.std_formatter);
+    print_part c
+  | Error e -> failwith ("cannot parse email: " ^ e)
+;;
+
 let () =
   let cfg = Yeh.Config.Config.instance () in
   let module Hey =
@@ -25,7 +55,12 @@ let () =
   printf "spam: %s\n" (Uri.to_string api.topics.spam);
   printf "trash: %s\n" (Uri.to_string api.topics.trash);
   printf "everything: %s\n\n" (Uri.to_string api.topics.everything); *)
-  Hey.Topic.all_topics api.imbox
-  |> List.iter ~f:(fun (x : Hey.Topic.t) ->
-    List.iter ~f:(printf "%Ld\n") (Hey.Topic.all_entries x.id))
+  let topics = Hey.Topic.all_topics api.imbox in
+  let tids = topics |> List.map ~f:(fun (x : Hey.Topic.t) -> x.id) in
+  let entries = Hey.Topic.all_entries (List.hd_exn tids) in
+  let msgs =
+    List.map ~f:(fun x -> Hey.Api.mk (Printf.sprintf "/messages/%Ld.txt" x)) entries
+  in
+  let raw = Hey.invoke (List.hd_exn msgs) in
+  parse_print raw
 ;;
